87abaed0d9378924a67ee75620f9a4bef55cfe90
Rohit Sinha <rohit@swirldslabs.com>
2023-03-11 09:15:44 -0800

    sanity tests

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ use ark_poly::{
 };
 use ark_std::{UniformRand, test_rng, ops::*};
 
+mod utils;
+
 type F = ark_bls12_381::Fr;
 
 struct Proof {
@@ -23,7 +25,34 @@ struct Proof {
     b_wff_q_of_r: F,
 }
 
-fn prove(weights: &Vec<u64>, bitmap: &Vec<u64>) -> Proof {
+fn main() {
+    //let secret_keys: Vec<F> = sample_secret_keys(n-1);
+    let weights: Vec<F> = vec![
+        F::from(2), 
+        F::from(3), 
+        F::from(4), 
+        F::from(5), 
+        F::from(4), 
+        F::from(3), 
+        F::from(2), 
+        F::from(0)-F::from(15)
+    ];
+    let bitmap: Vec<F> = vec![
+        F::from(1),
+        F::from(1), 
+        F::from(0), 
+        F::from(1), 
+        F::from(0), 
+        F::from(1), 
+        F::from(1), 
+        F::from(1)
+    ];
+
+    let π = prove(&weights, &bitmap);
+    verify(&π);
+}
+
+fn prove(weights: &Vec<F>, bitmap: &Vec<F>) -> Proof {
     // compute the nth root of unity
     let n: u64 = 8;
 
@@ -35,16 +64,16 @@ fn prove(weights: &Vec<u64>, bitmap: &Vec<u64>) -> Proof {
     let ωr: F = ω * r;
     let ω_pow_n_minus_1: F = ω.pow([n-1]);
 
-    let w_of_x = compute_w_poly(&weights, &bitmap);
-    let w_of_ωx = poly_domain_mult_ω(&w_of_x, &ω);
+    let w_of_x = compute_poly(&weights);
+    let w_of_ωx = utils::poly_domain_mult_ω(&w_of_x, &ω);
 
-    let b_of_x = compute_b_poly(&bitmap);
-    let b_of_ωx = poly_domain_mult_ω(&b_of_x, &ω);
+    let b_of_x = compute_poly(&bitmap);
+    let b_of_ωx = utils::poly_domain_mult_ω(&b_of_x, &ω);
 
     let psw_of_x = compute_psw_poly(&weights, &bitmap);
-    let psw_of_ωx = poly_domain_mult_ω(&psw_of_x, &ω);
+    let psw_of_ωx = utils::poly_domain_mult_ω(&psw_of_x, &ω);
 
-    let z_of_x = compute_vanishing_poly(n); //returns Z(X) = X^n - 1
+    let z_of_x = utils::compute_vanishing_poly(n); //returns Z(X) = X^n - 1
 
     //t(X) = ParSumW(ω · X) − ParSumW(X) − W(ω · X) · b(ω · X)
     let t_of_x = psw_of_ωx.sub(&psw_of_x).sub(&w_of_ωx.mul(&b_of_ωx));
@@ -88,236 +117,302 @@ fn verify(π: &Proof) {
     assert_eq!(π.b_of_ω_pow_n_minus_1, F::from(1));
 }
 
-fn main() {
-    // set n to the desired positive integer
-    // let n: u64 = 8;
-
-    // // create an evaluation domain of size n
-    // let domain: Radix2EvaluationDomain<F> = Radix2EvaluationDomain::<F>::new(8).unwrap();
-    // let mut evals = vec![];
-    // for i in 0..n {
-    //     evals.push(F::from(i));
-    // }
-    // let eval_form = Evaluations::from_vec_and_domain(evals, domain);
-    // let poly: DensePolynomial<F> = eval_form.interpolate();
-    // println!("degree of poly: {:?}", poly.degree());
-    // println!("poly coeff: {}", poly.coeffs[0]);
-    
-
-    // let group_gen = F::get_root_of_unity(n).unwrap();
-    // println!("The nth root of unity is: {:?}", group_gen);
-    // let omega_squared = group_gen.pow([n]);
-    // println!("The omega_squared of unity is: {:?}", omega_squared);
-    // // Check that it is indeed the 2^(log_size_of_group) root of unity.
-    // //debug_assert_eq!(group_gen.pow([size]), F::one());
-    
-
-    // compute the nth root of unity
-    let n: u64 = 8;
-    let domain = Radix2EvaluationDomain::<F>::new(8).unwrap();
-    let ω: F = domain.group_gen;
-    //println!("The nth root of unity is: {:?}", ω);
-    //println!("The omega_n_minus_1 of unity is: {:?}", ω.pow([n-1]));
-    //println!("The omega_n of unity is: {:?}", ω_pow_n_minus_1 * ω);
-
-    let weights: Vec<u64> = vec![2, 3, 4, 5, 4, 3, 2];
-    let bitmap: Vec<u64> = vec![1, 1, 0, 1, 0, 1, 1];
-
-    let w_of_x = compute_w_poly(&weights, &bitmap);
-    let w_of_ωx = poly_domain_mult_ω(&w_of_x, &ω);
-
-    let b_of_x = compute_b_poly(&bitmap);
-    let b_of_ωx = poly_domain_mult_ω(&b_of_x, &ω);
-
-    let psw_of_x = compute_psw_poly(&weights, &bitmap);
-    let psw_of_ωx = poly_domain_mult_ω(&psw_of_x, &ω);
-
-    //t(X) = ParSumW(ω · X) − ParSumW(X) − W(ω · X) · b(ω · X)
-    let t_of_x = psw_of_ωx.sub(&psw_of_x).sub(&w_of_ωx.mul(&b_of_ωx));
-    let z_of_x = compute_vanishing_poly(n); //returns Z(X) = X^n - 1
-    let q2_of_x = t_of_x.div(&z_of_x);
-
-    let t_of_x = b_of_x.mul(&b_of_x).sub(&b_of_x);
-    let q1_of_x = t_of_x.div(&z_of_x);
-    
-    test_poly_domain_mult(&w_of_x, &w_of_ωx, &ω);
-    test_poly_domain_mult(&b_of_x, &b_of_ωx, &ω);
-    test_poly_domain_mult(&psw_of_x, &psw_of_ωx, &ω);
-
-    sanity_test_psw(&w_of_x, &b_of_x, &psw_of_x, &q2_of_x);
-    sanity_test_b(&b_of_x, &q1_of_x);
-
-    let π = prove(&weights, &bitmap);
-    verify(&π);
-}
-
-fn test_poly_domain_mult(f_of_x: &DensePolynomial<F>, f_of_ωx: &DensePolynomial<F>, ω: &F) {
-    let mut rng = test_rng();
-    let r = F::rand(&mut rng);
-    let ωr: F = ω.clone() * r;
-
-    //println!("f(x) at ωr = {:?}", f_of_x.evaluate(&ωr));
-    //println!("f(ωx) at r = {:?}", f_of_ωx.evaluate(&r));
-    assert_eq!(f_of_x.evaluate(&ωr), f_of_ωx.evaluate(&r));
-}
-
-//returns t(X) = X^n - 1
-fn compute_vanishing_poly(n: u64) -> DensePolynomial<F> {
-    let mut coeffs = vec![];
-    for i in 0..n+1 {
-        if i == 0 {
-            coeffs.push(F::from(0) - F::from(1)); // -1
-        } else if i == n {
-            coeffs.push(F::from(1)); // X^n
-        } else {
-            coeffs.push(F::from(0));
-        }
-    }
-    DensePolynomial { coeffs }
-}
-
-fn poly_domain_mult_ω(f: &DensePolynomial<F>, ω: &F) -> DensePolynomial<F> {
-    let mut new_poly = f.clone();
-    for i in 1..(f.degree() + 1) { //we don't touch the zeroth coefficient
-        let ω_pow_i: F = ω.pow([i as u64]);
-        new_poly.coeffs[i] = new_poly.coeffs[i] * ω_pow_i;
-    }
-    new_poly
-}
-
-fn sanity_test_b(
-    b_of_x: &DensePolynomial<F>,
-    q_of_x: &DensePolynomial<F>
-) {
-
-    let mut rng = test_rng();
-    let r = F::rand(&mut rng);
-
-    let n: u64 = (b_of_x.degree() + 1) as u64;
-
-    let b_of_r = b_of_x.evaluate(&r);
-    let q_of_r = q_of_x.evaluate(&r);
-    let vanishing_of_r: F = r.pow([n]) - F::from(1);
-
-    //ParSumW(ωr) − ParSumW(r) − W(ωr) · b(ωr) = Q2(r) · (r^n − 1)
-    let tmp1 = b_of_r * b_of_r - b_of_r;
-    let tmp2 = q_of_r * vanishing_of_r;
-    //println!("ParSumW(ωr) - ParSumW(r) - W(ωr)·b(ωr) = {:?}", tmp1);
-    //println!("Q(r) · (r^n - 1) = {:?}", tmp2);
-    assert_eq!(tmp1, tmp2);
-
-}
-
-
-fn sanity_test_psw(
-    w_of_x: &DensePolynomial<F>,
-    b_of_x: &DensePolynomial<F>,
-    psw_of_x: &DensePolynomial<F>,
-    q_of_x: &DensePolynomial<F>
-) {
-
-    let mut rng = test_rng();
-    let r = F::rand(&mut rng);
-
-    let n: u64 = (b_of_x.degree() + 1) as u64;
-    let domain = Radix2EvaluationDomain::<F>::new(8).unwrap();
-    let ω: F = domain.group_gen;
-    let ω_pow_n_minus_1: F = ω.pow([n-1]);
-    let ωr: F = ω * r;
-
-    let psw_of_r = psw_of_x.evaluate(&r);
-    let psw_of_ωr = psw_of_x.evaluate(&ωr);
-    let w_of_ωr = w_of_x.evaluate(&ωr);
-    let b_of_ωr = b_of_x.evaluate(&ωr);
-    let q_of_r = q_of_x.evaluate(&r);
-    let vanishing_of_r: F = r.pow([n]) - F::from(1);
-
-    //ParSumW(ωr) − ParSumW(r) − W(ωr) · b(ωr) = Q2(r) · (r^n − 1)
-    let tmp1 = psw_of_ωr - psw_of_r - w_of_ωr * b_of_ωr;
-    let tmp2 = q_of_r * vanishing_of_r;
-    //println!("ParSumW(ωr) - ParSumW(r) - W(ωr)·b(ωr) = {:?}", tmp1);
-    //println!("Q(r) · (r^n - 1) = {:?}", tmp2);
-    assert_eq!(tmp1, tmp2);
-
-    //ParSumW(ωn−1) = 0
-    let psw_of_ω_pow_n_minus_1 = psw_of_x.evaluate(&ω_pow_n_minus_1);
-    //println!("ParSumW(ω^(n-1)) = {:?}", psw_of_ω_pow_n_minus_1);
-    assert_eq!(psw_of_ω_pow_n_minus_1, F::from(0));
-
-    //b(ωn−1) = 1
-    let b_of_ω_pow_n_minus_1 = b_of_x.evaluate(&ω_pow_n_minus_1);
-    assert_eq!(b_of_ω_pow_n_minus_1, F::from(1));
-
-}
-
-fn compute_w_poly(weights: &Vec<u64>, bitmap: &Vec<u64>) -> DensePolynomial<F> {
-    let num_parties = weights.len();
-    //we need n = #parties + 1, for nth root of unity
-    let n = num_parties + 1;
-    
+fn compute_poly(v: &Vec<F>) -> DensePolynomial<F> {
+    let n = v.len();
     let mut evals = vec![];
-    let mut total_weight = 0;
-    for i in 0..num_parties {
-        evals.push(F::from(weights[i]));
-        total_weight += if bitmap[i] == 1 { weights[i] } else { 0 };
+    for i in 0..n {
+        evals.push(v[i]);
     }
-    //last entry is the additive inverse of cumulative weight
-    evals.push(F::from(0) - F::from(total_weight));
 
     let domain = Radix2EvaluationDomain::<F>::new(n).unwrap();
     let eval_form = Evaluations::from_vec_and_domain(evals, domain);
     eval_form.interpolate()
 }
 
-fn compute_b_poly(bitmap: &Vec<u64>) -> DensePolynomial<F> {
-    let num_parties = bitmap.len();
-    //we need n = #parties + 1, for nth root of unity
-    let n = num_parties + 1;
-
+fn compute_psw_poly(weights: &Vec<F>, bitmap: &Vec<F>) -> DensePolynomial<F> {
+    let n = weights.len();
+    let mut parsum = F::from(0);
     let mut evals = vec![];
-    for i in 0..num_parties {
-        evals.push(F::from(bitmap[i]));
-    }
-    //last entry is 1 to activate the additive inverse of total weight
-    evals.push(F::from(1));
-
-    let domain = Radix2EvaluationDomain::<F>::new(n).unwrap();
-    let eval_form = Evaluations::from_vec_and_domain(evals, domain);
-    eval_form.interpolate()
-}
-
-fn compute_psw_poly(weights: &Vec<u64>, bitmap: &Vec<u64>) -> DensePolynomial<F> {
-    let num_parties = bitmap.len();
-    //we need n = #parties + 1, for nth root of unity
-    let n = num_parties + 1;
-
-    let mut parsum = 0;
-    let mut evals = vec![];
-    for i in 0..num_parties {
-        let w_i_b_i = if bitmap[i] == 1 { weights[i] } else { 0 };
+    for i in 0..n {
+        let w_i_b_i = bitmap[i] * weights[i];
         parsum += w_i_b_i;
-        evals.push(F::from(parsum));
+        evals.push(parsum);
     }
-    //last entry is 1 to activate the additive inverse of total weight
-    evals.push(F::from(0));
 
     let domain = Radix2EvaluationDomain::<F>::new(n).unwrap();
     let eval_form = Evaluations::from_vec_and_domain(evals, domain);
     eval_form.interpolate()    
 }
 
-// 1 at omega^i and 0 elsewhere on domain {omega^i}_{i \in [n]}
-fn _lagrange_poly(n: usize, i: usize) -> DensePolynomial<F> {
-    //todo: check n is a power of 2
-    let mut evals = vec![];
-    for j in 0..n {
-        let l_of_x: u64 = if i == j { 1 } else { 0 };
-        evals.push(F::from(l_of_x));
+
+
+#[cfg(test)]
+mod tests {
+    use crate::utils::compute_x_monomial;
+
+    use super::*;
+
+    fn sample_secret_keys(num_parties: usize) -> Vec<F> {
+        let mut rng = test_rng();
+        let mut keys = vec![];
+        for _ in 0..num_parties {
+            keys.push(F::rand(&mut rng));
+        }
+        keys.push(F::from(0));
+        keys
     }
 
-    //powers of nth root of unity
-    let domain = Radix2EvaluationDomain::<F>::new(n).unwrap();
-    let eval_form = Evaluations::from_vec_and_domain(evals, domain);
-    //interpolated polynomial over the n points
-    eval_form.interpolate()
+    fn aggregate_sk(sk: &Vec<F>, bitmap: &Vec<F>) -> F {
+        let n = sk.len();
+        let mut agg_sk = F::from(0);
+        for i in 0..sk.len() {
+            let l_i_of_x = utils::lagrange_poly(n, i);
+            let l_i_of_0 = l_i_of_x.evaluate(&F::from(0));
+            agg_sk += bitmap[i] * sk[i] * l_i_of_0;
+        }
+        agg_sk
+    }
+
+    fn sanity_test_poly_domain_mult(
+        f_of_x: &DensePolynomial<F>, 
+        f_of_ωx: &DensePolynomial<F>, 
+        ω: &F) {
+        let mut rng = test_rng();
+        let r = F::rand(&mut rng);
+        let ωr: F = ω.clone() * r;
+        assert_eq!(f_of_x.evaluate(&ωr), f_of_ωx.evaluate(&r));
+    }
+
+    fn sanity_test_b(
+        b_of_x: &DensePolynomial<F>,
+        q_of_x: &DensePolynomial<F>
+    ) {
+    
+        let mut rng = test_rng();
+        let r = F::rand(&mut rng);
+    
+        let n: u64 = (b_of_x.degree() + 1) as u64;
+    
+        let b_of_r = b_of_x.evaluate(&r);
+        let q_of_r = q_of_x.evaluate(&r);
+        let vanishing_of_r: F = r.pow([n]) - F::from(1);
+    
+        //ParSumW(ωr) − ParSumW(r) − W(ωr) · b(ωr) = Q2(r) · (r^n − 1)
+        let left = b_of_r * b_of_r - b_of_r;
+        let right = q_of_r * vanishing_of_r;
+        //println!("ParSumW(ωr) - ParSumW(r) - W(ωr)·b(ωr) = {:?}", tmp1);
+        //println!("Q(r) · (r^n - 1) = {:?}", tmp2);
+        assert_eq!(left, right);
+    
+    }
+    
+    fn sanity_test_psw(
+        w_of_x: &DensePolynomial<F>,
+        b_of_x: &DensePolynomial<F>,
+        psw_of_x: &DensePolynomial<F>,
+        q_of_x: &DensePolynomial<F>
+    ) {
+    
+        let mut rng = test_rng();
+        let r = F::rand(&mut rng);
+    
+        let n: u64 = (b_of_x.degree() + 1) as u64;
+        let domain = Radix2EvaluationDomain::<F>::new(8).unwrap();
+        let ω: F = domain.group_gen;
+        let ω_pow_n_minus_1: F = ω.pow([n-1]);
+        let ωr: F = ω * r;
+    
+        let psw_of_r = psw_of_x.evaluate(&r);
+        let psw_of_ωr = psw_of_x.evaluate(&ωr);
+        let w_of_ωr = w_of_x.evaluate(&ωr);
+        let b_of_ωr = b_of_x.evaluate(&ωr);
+        let q_of_r = q_of_x.evaluate(&r);
+        let vanishing_of_r: F = r.pow([n]) - F::from(1);
+    
+        //ParSumW(ωr) − ParSumW(r) − W(ωr) · b(ωr) = Q2(r) · (r^n − 1)
+        let tmp1 = psw_of_ωr - psw_of_r - w_of_ωr * b_of_ωr;
+        let tmp2 = q_of_r * vanishing_of_r;
+        //println!("ParSumW(ωr) - ParSumW(r) - W(ωr)·b(ωr) = {:?}", tmp1);
+        //println!("Q(r) · (r^n - 1) = {:?}", tmp2);
+        assert_eq!(tmp1, tmp2);
+    
+        //ParSumW(ωn−1) = 0
+        let psw_of_ω_pow_n_minus_1 = psw_of_x.evaluate(&ω_pow_n_minus_1);
+        //println!("ParSumW(ω^(n-1)) = {:?}", psw_of_ω_pow_n_minus_1);
+        assert_eq!(psw_of_ω_pow_n_minus_1, F::from(0));
+    
+        //b(ωn−1) = 1
+        let b_of_ω_pow_n_minus_1 = b_of_x.evaluate(&ω_pow_n_minus_1);
+        assert_eq!(b_of_ω_pow_n_minus_1, F::from(1));
+    
+    }
+
+    #[test]
+    fn sanity_test_public_part() {
+        // compute the nth root of unity
+        let n: u64 = 8;
+        let domain = Radix2EvaluationDomain::<F>::new(8).unwrap();
+        let ω: F = domain.group_gen;
+        //println!("The nth root of unity is: {:?}", ω);
+        //println!("The omega_n_minus_1 of unity is: {:?}", ω.pow([n-1]));
+        //println!("The omega_n of unity is: {:?}", ω_pow_n_minus_1 * ω);
+
+        let weights: Vec<F> = vec![
+            F::from(2), 
+            F::from(3), 
+            F::from(4), 
+            F::from(5), 
+            F::from(4), 
+            F::from(3), 
+            F::from(2), 
+            F::from(0)-F::from(15)
+        ];
+        let bitmap: Vec<F> = vec![
+            F::from(1),
+            F::from(1), 
+            F::from(0), 
+            F::from(1), 
+            F::from(0), 
+            F::from(1), 
+            F::from(1), 
+            F::from(1)
+        ];
+
+        let w_of_x = compute_poly(&weights);
+        let w_of_ωx = utils::poly_domain_mult_ω(&w_of_x, &ω);
+
+        let b_of_x = compute_poly(&bitmap);
+        let b_of_ωx = utils::poly_domain_mult_ω(&b_of_x, &ω);
+
+        let psw_of_x = compute_psw_poly(&weights, &bitmap);
+        let psw_of_ωx = utils::poly_domain_mult_ω(&psw_of_x, &ω);
+
+        //t(X) = ParSumW(ω · X) − ParSumW(X) − W(ω · X) · b(ω · X)
+        let t_of_x = psw_of_ωx.sub(&psw_of_x).sub(&w_of_ωx.mul(&b_of_ωx));
+        let z_of_x = utils::compute_vanishing_poly(n); //returns Z(X) = X^n - 1
+        let q2_of_x = t_of_x.div(&z_of_x);
+
+        let t_of_x = b_of_x.mul(&b_of_x).sub(&b_of_x);
+        let q1_of_x = t_of_x.div(&z_of_x);
+        
+        sanity_test_poly_domain_mult(&w_of_x, &w_of_ωx, &ω);
+        sanity_test_poly_domain_mult(&b_of_x, &b_of_ωx, &ω);
+        sanity_test_poly_domain_mult(&psw_of_x, &psw_of_ωx, &ω);
+
+        sanity_test_psw(&w_of_x, &b_of_x, &psw_of_x, &q2_of_x);
+        sanity_test_b(&b_of_x, &q1_of_x);
+    }
+
+    fn sanity_test_pssk(
+        sk_of_x: &DensePolynomial<F>,
+        b_of_x: &DensePolynomial<F>,
+        q1_of_x: &DensePolynomial<F>,
+        q2_of_x: &DensePolynomial<F>,
+        agg_sk: &F
+    ) {
+        let mut rng = test_rng();
+        let r = F::rand(&mut rng);
+        let n: u64 = 8;
+
+        //SK(x) · B(x) − aSK = Q1(x) · Z(x) + Q2(x) · x
+        let sk_of_r = sk_of_x.evaluate(&r);
+        let b_of_r = b_of_x.evaluate(&r);
+        let q1_of_r = q1_of_x.evaluate(&r);
+        let z_of_r: F = r.pow([n]) - F::from(1);
+        let q2_of_r = q2_of_x.evaluate(&r);
+
+        let left = sk_of_r * b_of_r;
+        let right = (q1_of_r * z_of_r) + (q2_of_r * r) + agg_sk;
+        assert_eq!(left, right);
+    
+    }
+
+    #[test]
+    fn sanity_test_secret_part() {
+        //let weights: Vec<u64> = vec![2, 3, 4, 5, 4, 3, 2];
+        let bitmap: Vec<F> = vec![
+            F::from(1),
+            F::from(1), 
+            F::from(0), 
+            F::from(1), 
+            F::from(0), 
+            F::from(1), 
+            F::from(1), 
+            F::from(1)
+        ];
+
+        let secret_keys: Vec<F> = sample_secret_keys(7); //size 8
+
+        let agg_sk = aggregate_sk(&secret_keys, &bitmap);
+        println!("agg_sk = {:?}", agg_sk);
+        let sk_of_x = compute_poly(&secret_keys);
+        let b_of_x = compute_poly(&bitmap);
+        let q1_of_x = compute_pssk_q1_poly(&secret_keys, &bitmap);
+        let q2_of_x = compute_pssk_q2_poly(&secret_keys, &bitmap);
+
+        sanity_test_pssk(&sk_of_x, &b_of_x, &q1_of_x, &q2_of_x, &agg_sk);
+    }
+
+    fn compute_pssk_q1_poly(
+        sk: &Vec<F>, 
+        bitmap: &Vec<F>
+    ) -> DensePolynomial<F> {
+        let n = sk.len();
+        let z_of_x = utils::compute_vanishing_poly(n as u64);
+        //Li(x) · Li(x) − Li(x) / Z(x)
+        let mut q1 = utils::compute_constant_poly(&F::from(0));
+
+        for i in 0..n {
+            if bitmap[i] == F::from(0) { continue; }
+
+            let l_i_of_x = utils::lagrange_poly(n, i);
+            let num = l_i_of_x.clone().mul(&l_i_of_x);
+            let num = num.sub(&l_i_of_x);
+            let f_i = num.div(&z_of_x.clone());
+            let sk_i_f_i = utils::poly_eval_mult_c(&f_i, &sk[i]);
+
+            q1 = q1.add(sk_i_f_i);
+
+            let mut q1_inner = utils::compute_constant_poly(&F::from(0));
+            for j in 0..n {
+                if i == j { continue; } //i != j
+
+                let l_j_of_x = utils::lagrange_poly(n, j);
+                let num = l_j_of_x.mul(&l_i_of_x);
+                let f_j = num.div(&z_of_x.clone());
+                let sk_j_f_j = utils::poly_eval_mult_c(&f_j, &sk[j]);
+
+                q1_inner = q1_inner.clone().add(sk_j_f_j);
+            }
+
+            q1 = q1.clone().add(q1_inner);
+        }
+        q1
+    }
+
+    fn compute_pssk_q2_poly(
+        sk: &Vec<F>, 
+        bitmap: &Vec<F>
+    ) -> DensePolynomial<F> {
+        let n = sk.len();
+        let x_monomial = compute_x_monomial();
+
+        let mut q2 = utils::compute_constant_poly(&F::from(0));
+
+        for i in 0..n {
+            if bitmap[i] == F::from(0) { continue; }
+
+            let l_i_of_x = utils::lagrange_poly(n, i);
+            let l_i_of_0 = l_i_of_x.evaluate(&F::from(0));
+            let l_i_of_0 = utils::compute_constant_poly(&l_i_of_0);
+            let num = l_i_of_x.sub(&l_i_of_0);
+            let den = x_monomial.clone();
+            let f_i = num.div(&den);
+            let sk_i_f_i = utils::poly_eval_mult_c(&f_i, &sk[i]);
+
+            q2 = q2.add(sk_i_f_i);
+        }
+        q2
+    }
+
 }
