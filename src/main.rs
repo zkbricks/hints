160c9f7cedfd3da9d65ece535d26a7738ef4ffa1
Rohit Sinha <rohit@swirldslabs.com>
2023-03-31 12:04:44 -0700

    letting the verifier evaluate the lagrange polynomials as it is constant time

--- src/main.rs
+++ src/main.rs
@@ -33,31 +33,15 @@ struct Proof {
     merged_proof: G1,
 
     psw_of_r: F,
-    //psw_of_r_proof: G1,
 
     psw_of_r_div_ω: F,
     psw_of_r_div_ω_proof: G1,
-
     w_of_r: F,
-    //w_of_r_proof: G1,
-
     b_of_r: F,
-    //b_of_r_proof: G1,
-
     psw_wff_q_of_r: F,
-    //psw_wff_q_of_r_proof: G1,
-
-    l_n_minus_1_of_r: F,
-    //l_n_minus_1_of_r_proof: G1,
-
     psw_check_q_of_r: F,
-    //psw_check_q_of_r_proof: G1,
-
     b_wff_q_of_r: F,
-    //b_wff_q_of_r_proof: G1,
-
     b_check_q_of_r: F,
-    //b_check_q_of_r_proof: G1,
 
     psw_of_x_com: G1,
     b_of_x_com: G1,
@@ -285,7 +269,6 @@ fn prove(
     let w_of_r_proof = KZG::compute_opening_proof(&params, &w_of_x, &r).unwrap();
     let b_of_r_proof = KZG::compute_opening_proof(&params, &b_of_x, &r).unwrap();
     let psw_wff_q_of_r_proof = KZG::compute_opening_proof(&params, &psw_wff_q_of_x, &r).unwrap();
-    let l_n_minus_1_of_r_proof = KZG::compute_opening_proof(&params, &l_n_minus_1_of_x, &r).unwrap();
     let psw_check_q_of_r_proof = KZG::compute_opening_proof(&params, &psw_check_q_of_x, &r).unwrap();
     let b_wff_q_of_r_proof = KZG::compute_opening_proof(&params, &b_wff_q_of_x, &r).unwrap();
     let b_check_q_of_r_proof = KZG::compute_opening_proof(&params, &b_check_q_of_x, &r).unwrap();
@@ -294,10 +277,9 @@ fn prove(
         + w_of_r_proof.mul(r.pow([1]))
         + b_of_r_proof.mul(r.pow([2]))
         + psw_wff_q_of_r_proof.mul(r.pow([3]))
-        + l_n_minus_1_of_r_proof.mul(r.pow([4]))
-        + psw_check_q_of_r_proof.mul(r.pow([5]))
-        + b_wff_q_of_r_proof.mul(r.pow([6]))
-        + b_check_q_of_r_proof.mul(r.pow([7]))).into();
+        + psw_check_q_of_r_proof.mul(r.pow([4]))
+        + b_wff_q_of_r_proof.mul(r.pow([5]))
+        + b_check_q_of_r_proof.mul(r.pow([6]))).into();
 
     Proof {
         agg_pk: agg_pk.clone(),
@@ -309,29 +291,13 @@ fn prove(
         psw_of_r_div_ω_proof: KZG::compute_opening_proof(&params, &psw_of_x, &r_div_ω).unwrap(),
 
         psw_of_r: psw_of_x.evaluate(&r),
-        //psw_of_r_proof: psw_of_r_proof,
-
         w_of_r: w_of_x.evaluate(&r),
-        //w_of_r_proof: w_of_r_proof,
-
         b_of_r: b_of_x.evaluate(&r),
-        //b_of_r_proof: b_of_r_proof,
-
         psw_wff_q_of_r: psw_wff_q_of_x.evaluate(&r),
-        //psw_wff_q_of_r_proof: psw_wff_q_of_r_proof,
-
-        l_n_minus_1_of_r: l_n_minus_1_of_x.evaluate(&r),
-        //l_n_minus_1_of_r_proof: l_n_minus_1_of_r_proof,
-
         psw_check_q_of_r: psw_check_q_of_x.evaluate(&r),
-        //psw_check_q_of_r_proof: psw_check_q_of_r_proof,
-
         b_wff_q_of_r: b_wff_q_of_x.evaluate(&r),
-        //b_wff_q_of_r_proof: b_wff_q_of_r_proof,
-
         b_check_q_of_r: b_check_q_of_x.evaluate(&r),
-        //b_check_q_of_r_proof: b_check_q_of_r_proof,
-
+        
         merged_proof: merged_proof.into(),
 
         psw_of_x_com: KZG::commit_g1(&params, &psw_of_x).unwrap(),
@@ -366,20 +332,10 @@ fn verify_openings(vp: &VerifierPreprocessing, π: &Proof) {
     let adjustment_com = vp.l_n_minus_1_of_x_com.mul(adjustment);
     let w_of_x_com: G1 = (vp.w_of_x_com + adjustment_com).into();
 
-    // verify_opening(vp, &w_of_x_com, &π.r, &π.w_of_r, &π.w_of_r_proof);
-    // verify_opening(vp, &π.psw_of_x_com, &π.r, &π.psw_of_r, &π.psw_of_r_proof);
-    // verify_opening(vp, &π.b_of_x_com, &π.r, &π.b_of_r, &π.b_of_r_proof);
-    // verify_opening(vp, &π.psw_wff_q_of_x_com, &π.r, &π.psw_wff_q_of_r, &π.psw_wff_q_of_r_proof);
-    // verify_opening(vp, &vp.l_n_minus_1_of_x_com, &π.r, &π.l_n_minus_1_of_r, &π.l_n_minus_1_of_r_proof);
-    // verify_opening(vp, &π.psw_check_q_of_x_com, &π.r, &π.psw_check_q_of_r, &π.psw_check_q_of_r_proof);
-    // verify_opening(vp, &π.b_wff_q_of_x_com, &π.r, &π.b_wff_q_of_r, &π.b_wff_q_of_r_proof);
-    // verify_opening(vp, &π.b_check_q_of_x_com, &π.r, &π.b_check_q_of_r, &π.b_check_q_of_r_proof);
-
     let psw_of_r_argument = π.psw_of_x_com - vp.g_0.clone().mul(π.psw_of_r).into_affine();
     let w_of_r_argument = w_of_x_com - vp.g_0.clone().mul(π.w_of_r).into_affine();
     let b_of_r_argument = π.b_of_x_com - vp.g_0.clone().mul(π.b_of_r).into_affine();
     let psw_wff_q_of_r_argument = π.psw_wff_q_of_x_com - vp.g_0.clone().mul(π.psw_wff_q_of_r).into_affine();
-    let l_n_minus_1_of_r_argument = vp.l_n_minus_1_of_x_com - vp.g_0.clone().mul(π.l_n_minus_1_of_r).into_affine();
     let psw_check_q_of_r_argument = π.psw_check_q_of_x_com - vp.g_0.clone().mul(π.psw_check_q_of_r).into_affine();
     let b_wff_q_of_r_argument = π.b_wff_q_of_x_com - vp.g_0.clone().mul(π.b_wff_q_of_r).into_affine();
     let b_check_q_of_r_argument = π.b_check_q_of_x_com - vp.g_0.clone().mul(π.b_check_q_of_r).into_affine();
@@ -388,10 +344,9 @@ fn verify_openings(vp: &VerifierPreprocessing, π: &Proof) {
         + w_of_r_argument.mul(π.r.pow([1]))
         + b_of_r_argument.mul(π.r.pow([2]))
         + psw_wff_q_of_r_argument.mul(π.r.pow([3]))
-        + l_n_minus_1_of_r_argument.mul(π.r.pow([4]))
-        + psw_check_q_of_r_argument.mul(π.r.pow([5]))
-        + b_wff_q_of_r_argument.mul(π.r.pow([6]))
-        + b_check_q_of_r_argument.mul(π.r.pow([7]))).into_affine();
+        + psw_check_q_of_r_argument.mul(π.r.pow([4]))
+        + b_wff_q_of_r_argument.mul(π.r.pow([5]))
+        + b_check_q_of_r_argument.mul(π.r.pow([6]))).into_affine();
 
     let lhs = <Bls12_381 as Pairing>::pairing(
         merged_argument, 
@@ -408,10 +363,18 @@ fn verify_openings(vp: &VerifierPreprocessing, π: &Proof) {
 }
 
 fn verify(vp: &VerifierPreprocessing, π: &Proof) {
+    let domain = Radix2EvaluationDomain::<F>::new(vp.n as usize).unwrap();
+    let ω: F = domain.group_gen;
+
     verify_openings(vp, π);
 
     let n: u64 = vp.n as u64;
     let vanishing_of_r: F = π.r.pow([n]) - F::from(1);
+
+    // compute L_i(r) using the relation L_i(x) = Z_V(x) / ( Z_V'(x) (x - ω^i) )
+    // where Z_V'(x)^-1 = x / N for N = |V|.
+    let ω_pow_n_minus_1 = ω.pow([n-1]);
+    let l_n_minus_1_of_r = (ω_pow_n_minus_1 / F::from(n)) * (vanishing_of_r / (π.r - ω_pow_n_minus_1));
 
     //assert polynomial identity for the secret part
     let lhs = <Bls12_381 as Pairing>::pairing(&π.b_of_x_com, &vp.sk_of_x_com);
@@ -430,7 +393,7 @@ fn verify(vp: &VerifierPreprocessing, π: &Proof) {
 
     //Ln−1(X) · ParSumW(X) = Z(X) · Q2(X)
     //TODO: compute l_n_minus_1_of_r in verifier -- dont put it in the proof.
-    let lhs = π.l_n_minus_1_of_r * π.psw_of_r;
+    let lhs = l_n_minus_1_of_r * π.psw_of_r;
     let rhs = vanishing_of_r * π.psw_check_q_of_r;
     assert_eq!(lhs, rhs);
 
@@ -440,7 +403,7 @@ fn verify(vp: &VerifierPreprocessing, π: &Proof) {
     assert_eq!(lhs, rhs);
 
     //Ln−1(X) · (b(X) − 1) = Z(X) · Q4(X)
-    let lhs = π.l_n_minus_1_of_r * (π.b_of_r - F::from(1));
+    let lhs = l_n_minus_1_of_r * (π.b_of_r - F::from(1));
     let rhs = vanishing_of_r * π.b_check_q_of_r;
     assert_eq!(lhs, rhs);
 
